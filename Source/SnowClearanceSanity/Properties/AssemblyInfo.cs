5d4ae6548cd4d67ba5e71ac2488d323f99317959
emipa606 <emil.offermo@litet.net>
2020-12-09 19:39:52 +0100

    First publish

---
+++ Source/SnowClearanceSanity/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.6864.6722")]
+[assembly: AssemblyTitle("SnowClearanceSanity")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("${AuthorCopyright}")]
+[assembly: AssemblyTrademark("")]
---
+++ Source/SnowClearanceSanity/SnowClearanceSanity.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace SnowClearanceSanity
+{
+    class SnowClearanceSanity : Mod
+    {
+        public SnowClearanceSanity(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony("net.marvinkosh.rimworld.mod.snowclearancesanity");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(WorkGiver_ClearSnow), "HasJobOnCell")]
+    class ClearanceSanity
+    {
+        public static void Postfix(ref bool __result, Pawn pawn, IntVec3 c, bool forced)
+        {
+
+            if (!__result)
+            {
+                return;
+            }
+            if (forced)
+            {
+                return;
+            }
+            if (pawn.Map.weatherManager.SnowRate >= SnowGentleRate && !c.Roofed(pawn.Map))
+            {
+                __result = false;
+            }
+        }
+
+        private const float SnowGentleRate = 0.8f;
+    }
+
+}
