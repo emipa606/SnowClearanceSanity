5646b4279139437799211dc97e007ed1592fcfef
emipa606 <emil.offermo@litet.net>
2021-07-13 21:28:20 +0200

    Mod updated for 1.3 and passed autotests

---
+++ Source/SnowClearanceSanity/ClearanceSanity.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace SnowClearanceSanity
+{
+    [HarmonyPatch(typeof(WorkGiver_ClearSnow), "HasJobOnCell")]
+    internal class ClearanceSanity
+    {
+        private const float SnowGentleRate = 0.8f;
+
+        public static void Postfix(ref bool __result, Pawn pawn, IntVec3 c, bool forced)
+        {
+            if (!__result)
+            {
+                return;
+            }
+
+            if (forced)
+            {
+                return;
+            }
+
+            if (pawn.Map.weatherManager.SnowRate >= SnowGentleRate && !c.Roofed(pawn.Map))
+            {
+                __result = false;
+            }
+        }
+    }
+}
--- Source/SnowClearanceSanity/SnowClearanceSanity.cs
+++ Source/SnowClearanceSanity/SnowClearanceSanity.cs
@@ -1,10 +1,9 @@
 ﻿using HarmonyLib;
-using RimWorld;
 using Verse;
 
 namespace SnowClearanceSanity
 {
-    class SnowClearanceSanity : Mod
+    internal class SnowClearanceSanity : Mod
     {
         public SnowClearanceSanity(ModContentPack content) : base(content)
         {
@@ -12,28 +11,4 @@ namespace SnowClearanceSanity
             harmony.PatchAll();
         }
     }
-
-    [HarmonyPatch(typeof(WorkGiver_ClearSnow), "HasJobOnCell")]
-    class ClearanceSanity
-    {
-        public static void Postfix(ref bool __result, Pawn pawn, IntVec3 c, bool forced)
-        {
-
-            if (!__result)
-            {
-                return;
-            }
-            if (forced)
-            {
-                return;
-            }
-            if (pawn.Map.weatherManager.SnowRate >= SnowGentleRate && !c.Roofed(pawn.Map))
-            {
-                __result = false;
-            }
-        }
-
-        private const float SnowGentleRate = 0.8f;
-    }
-
 }
